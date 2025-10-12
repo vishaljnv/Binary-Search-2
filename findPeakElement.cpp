d1f2cd5664c12c94302eed71f8000c1eb10a3c3b
Vishal Yarabandi <33473675+vishaljnv@users.noreply.github.com>
2025-10-12 11:24:47 +0530

    Find Peak Element Complete

---
+++ findPeakElement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        int low = 0, high = nums.size() - 1, mid;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
